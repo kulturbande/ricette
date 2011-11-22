c792b65a1a2d1f9817ea633c5e93f56b782c2ce4
Sascha Karnatz <sascha@kulturbande.de>
2011-11-22 22:10:13 +0100

    switch header (not ready yet)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def default_title(default_title)
+    content_for?(:title) ? content_for(:title) : default_title
+  end
+
+  def title(page_title)
+    content_for(:title) { page_title }
+  end
+
 end
